a07e2a724f556dd8df6250943d8942ba0afb30fb
jacef790 <jace.feinstein@live.com>
2022-04-04 23:53:12 -0600

    Added question

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # HW-04-Quiz
 html, css, some js added
 initial upload
+
+questions added to question object
--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-var startBtnEl = document.getElementById('start-btn')
-var questionContainerEl = document.getElementById('question-container')
-var currentQues
-var shuffleQues
-start.addEventListener('click', startQuiz)
+var startBtnEl = document.getElementById('start-btn');
+var questionContainerEl = document.getElementById('question-container');
+var nextButtonEl = document.getElementById('next');
+var currentQues;
+var shuffleQues;
+start.addEventListener('click', startQuiz);
+nextButtonEl.addEventListener('click', nextQues());
 
 
 function startQuiz() {
     start.classList.add('hide');
     questionContainerEl.classList.remove('hide');
-    currentQuesIndex = 0
+    currentQuesIndex = 0;
     nextQues();
 
 
 }
 
 function nextQues() {
+    
+    // nextButtonEl.classList.remove('hide');
 
 }
 
@@ -30,6 +34,46 @@ var questions = [
             {text: 'BLT', correct: false},
             {text: 'PBJ', correct: true},
             {text: 'Chicken Salad', correct: false}
+        ]
+
+    },
+    {
+    question: 'How Many Super Bowls Have the Dallas Cowboys Won?',
+        answer: [
+            {text: '2', correct: false},
+            {text: '3', correct: false},
+            {text: '4', correct: false},
+            {text: '5', correct: true}
+        ]
+
+    },
+    {
+    question: 'Which is the Best Call of Duty?',
+        answer: [
+            {text: 'Modern Warfare 2', correct: false},
+            {text: 'Black Ops 2', correct: true},
+            {text: 'Black Ops', correct: false},
+            {text: 'Advanced Warfare ', correct: false}
+        ]
+
+    },
+    {
+    question: 'I am Keyser Soze?',
+        answer: [
+            {text: 'True', correct: false},
+            {text: 'False', correct: false},
+            {text: 'True, but again', correct: true},
+            {text: 'False, but maybe not', correct: false}
+        ]
+
+    },
+    {
+    question: 'Which is the Scariest?',
+        answer: [
+            {text: 'Clowns', correct: true},
+            {text: 'Zombies', correct: false},
+            {text: 'Vampires', correct: false},
+            {text: 'Zombie ClownPires', correct: false}
         ]
 
     }
